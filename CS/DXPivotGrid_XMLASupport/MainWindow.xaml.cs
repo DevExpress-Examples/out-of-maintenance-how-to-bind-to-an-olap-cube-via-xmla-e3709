13a208021870e80dfe7904c416adf69843fa04a1
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXPivotGrid_XMLASupport/MainWindow.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace DXPivotGrid_XMLASupport {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ VB/DXPivotGrid_XMLASupport/MainWindow.xaml.vb
@@ -0,0 +1,11 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+
+Namespace DXPivotGrid_XMLASupport
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
+End Namespace
